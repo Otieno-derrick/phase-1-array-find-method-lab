713fdf395c074ec1e991d4e90b7a430381e6aecc
Otieno-derrick <otieno.derrick@student.moringaschool.com>
2022-06-16 17:33:44 +0300

    complete

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // code your solution here
 function superbowlWin(record) {
-    const result = record.find(record => record.result === "W");
-    return result ? result.year: undefined;
+    let result = record.find(record => record.result === "W");
+    return result ? result.year : undefined;
 }
+
 
 
